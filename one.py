8ad6cf534c0c4d036f045cb44fdcf2a1292b4ec5
mageshwarant <48029648+mageshwarant@users.noreply.github.com>
2019-07-14 02:20:35 +0530

    first commit on xlrd

--- one.py
+++ one.py
@@ -3,27 +3,37 @@
 # second ="two"
 # maintain(first)
 # maintain(second)
-
-mylist = ["Magesh", 2, 3, 4, 5]
-
-print(mylist)
-
-myfile = open("maegsh1.txt", "w")
-
-for i in mylist:
-    myfile.write(str(i) + "\n")
-
-myfile.close()
-
-
-myfile = open("maegsh1.txt", "r")
-
-print(myfile.readline())
-print(myfile.readline())
-print(myfile.readline())
-print(myfile.readline())
-
+#
+# mylist = ["Magesh", 2, 3, 4, 5]
+#
+# print(mylist)
+#
+# myfile = open("maegsh1.txt", "w")
+#
+# for i in mylist:
+#     myfile.write(str(i) + "\n")
+#
+# myfile.close()
+#
+#
+# myfile = open("maegsh1.txt", "r")
+#
+# print(myfile.readline())
+# print(myfile.readline())
+# print(myfile.readline())
+# print(myfile.readline())
+#
 
 import xlrd as xl
 
-xl.open_workbook("")
+reader = xl.open_workbook("C:\\Users\\hp\\Desktop\\Book2.xls")
+sheetno = reader.sheet_by_index(0)
+
+print(sheetno.nrows)
+print(type(sheetno.ncols))
+
+list = []
+for i in range(0,sheetno.nrows):
+    list.append(sheetno.cell_value(i,0))
+
+print(len(list))
